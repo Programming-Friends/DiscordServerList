4ace21debcd3cf5221917a9035d3cca536d05f6b
Kyle Givler <xvegan88x@gmail.com>
2021-08-28 17:43:09 -0400

    Add EmailSenderfor issue #4

---
+++ DiscordServerList_MVC/DiscordServerList_MVC/Services/EmailSender.cs
@@ -0,0 +1,67 @@
+﻿/*
+MIT License
+Copyright(c) 2021 Kyle Givler
+https://github.com/JoyfulReaper
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+
+using DiscordServerList_MVC.Models;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using System.Threading.Tasks;
+
+namespace DiscordServerList_MVC.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly MailSettings _mailSettings;
+
+        public EmailSender(IOptions<MailSettings> mailSettings)
+        {
+            _mailSettings = mailSettings.Value;
+        }
+
+        public async Task SendEmailAsync(string emailTo, string subject, string htmlMessage)
+        {
+            var email = new MimeMessage();
+            email.Sender = MailboxAddress.Parse(_mailSettings.Mail);
+            email.To.Add(MailboxAddress.Parse(emailTo));
+            email.Subject = subject;
+
+            var builder = new BodyBuilder();
+            builder.HtmlBody = htmlMessage;
+
+            email.Body = builder.ToMessageBody();
+
+            await DoEmailSend(email);
+        }
+
+        private async Task DoEmailSend(MimeMessage email)
+        {
+            using var smtp = new SmtpClient();
+            smtp.Connect(_mailSettings.Host, _mailSettings.Port, SecureSocketOptions.StartTls);
+            smtp.Authenticate(_mailSettings.Mail, _mailSettings.Password);
+
+            await smtp.SendAsync(email);
+            smtp.Disconnect(true);
+        }
+    }
+}
